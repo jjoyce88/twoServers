2bf528b674d9e6c1a92a4f0f0b2213188b5c095e
John Joyce <jjoyce88>
2018-01-30 19:51:09 -0500

    fixed errors

--- twoServers.js
+++ twoServers.js
@@ -23,13 +23,13 @@ var server1 = http.createServer(handleRequest1);
 var server2 = http.createServer(handleRequest2);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(PORT1, function() {
+server1.listen(PORT1, function() {
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT1);
 });
 
-server.listen(PORT2, function()
+server2.listen(PORT2, function()
 {
 	console.log("Server listening on: http://localhost:" + PORT2);
 });
